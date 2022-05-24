e6955d6f2160e92b948ac82bb90e3d5a97b93c8e
MoralesEspina <60724084+MoralesEspina@users.noreply.github.com>
2022-05-23 18:16:33 -0600

    Modelo de Curriculum

---
+++ internal/models/curriculum.model.go
@@ -0,0 +1,21 @@
+package models
+
+import "github.com/Mynor2397/sqlnulls"
+
+// Curriculum implenta el modelo de la base de datos
+type Curriculum struct {
+	UUID           sqlnulls.NullString `json:"uuid,omitempty"`
+	Direction      sqlnulls.NullString `json:"direction,omitempty"`
+	Country        sqlnulls.NullString `json:"country,omitempty"`
+	HomePhone      sqlnulls.NullString `json:"homephone,omitempty"`
+	BornPlace      sqlnulls.NullString `json:"bornPlace,omitempty"`
+	Nacionality    sqlnulls.NullString `json:"nacionality,omitempty"`
+	Municipality   sqlnulls.NullString `json:"municipality,omitempty"`
+	Village 	   sqlnulls.NullString `json:"village,omitempty"`
+	WorkPhone      sqlnulls.NullString `json:"workPhone,omitempty"`
+	Age    		   sqlnulls.NullString `json:"age,omitempty"`
+	CivilStatus    sqlnulls.NullString `json:"civilStatus,omitempty"`
+	Etnia          sqlnulls.NullString `json:"etnia,omitempty"`
+	Passport       sqlnulls.NullString `json:"passport,omitempty"`
+	License        sqlnulls.NullString `json:"license,omitempty"`
+}
