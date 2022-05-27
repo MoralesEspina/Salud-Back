20cd42d7777810a5cd278dd44cc45d147ffb0143
MoralesEspina <60724084+MoralesEspina@users.noreply.github.com>
2022-05-26 21:05:00 -0600

    Agregando GetOneUser

--- internal/models/rolUser.model.go
+++ internal/models/rolUser.model.go
@@ -1,7 +1,5 @@
 package models
 
 type Rols struct {
-	ID          string `json:"id"`
-	Role        string `json:"role,omitempty"`
-	Description string `json:"description,omitempty"`
+	Role string `json:"role,omitempty"`
 }
